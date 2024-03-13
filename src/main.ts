c36cca38093728fae8a298b96da27bca75148556
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-03-13 08:24:25 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import './style.css'
+//import { setupCounter } from './counter.ts'
+//import './topics/01-basic-types.ts'
+import './topics/02-object-interfaces'
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+   
+    <p >Hola mundo</p>
+    
+  </div>
+`
+console.log('hola mundo')
+//setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,16 @@
+// si no va a cambiar, es const
+const name: string = 'Isa';
+
+let hpPoints : number | string = 80;
+
+hpPoints = 'FULL';
+
+let hpPoints2 : number | 'FULL' = 80;
+
+hpPoints2 = 'FULL';
+
+const isAlive : boolean = true;
+
+console.log(name, hpPoints, hpPoints2, isAlive)
+
+export {}
