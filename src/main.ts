5c7495efeed72fde1eae482478589167ae740ed0
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-03-13 08:42:46 +0100

    funciones basicas

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 //import { setupCounter } from './counter.ts'
 //import './topics/01-basic-types.ts'
 import './topics/02-object-interfaces'
+import './topics/03-functions.ts'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
    
