0e096ba75a364d4dc0973d7cd8181bf6fad32dcd
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-03-13 09:04:52 +0100

    interfacs

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -9,11 +9,33 @@ function multiply(firstNumber: number, secondNumber?: number, base:number=2){
     return firstNumber * base;
 }
 
-
+/* 
 const result : number = addNumbers(1,2)
 const result2 : string = addNumbersArrow(1,2)
 const multiplyResult: number = multiply(5);
 
 console.log({result})
 console.log({result2})
-console.log({multiplyResult})
+console.log({multiplyResult}) */
+
+
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
+
+const healCharacter = (character: Character,amount: number) => {
+    character.hp += amount;
+}
+
+const strider: Character = {
+    name: 'Strider',
+    hp: 70,
+    showHp: () => {
+        console.log(`Puntos de vida ${strider.hp}`);
+    }
+}
+healCharacter(strider,130)
+strider.showHp();
+export{}
